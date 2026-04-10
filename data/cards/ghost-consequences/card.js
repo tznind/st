8d12a5c231c5ba47e10df2933c0ead4f16904c78
tznind <tznind@dundee.ac.uk>
2026-04-10 07:40:24 +0100

    Add consequences for ghost and revenant

---
+++ data/cards/ghost-consequences/card.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+
+  function initializeGhostConsequences(container) {
+    // Wire up X buttons
+    container.querySelectorAll('.mark-x-btn').forEach(function(btn) {
+      var crossId = btn.getAttribute('data-cross');
+      var checkbox = container.querySelector('#' + crossId);
+      if (!checkbox) return;
+
+      updateButton(btn, checkbox.checked);
+
+      btn.addEventListener('click', function() {
+        checkbox.checked = !checkbox.checked;
+        checkbox.dispatchEvent(new Event('change', { bubbles: true }));
+        updateButton(btn, checkbox.checked);
+      });
+    });
+
+    // Hide untaken: only the tick checkbox determines visibility
+    var hideUntakenEl = document.getElementById('hide_untaken');
+
+    function updateVisibility() {
+      var hideUntaken = hideUntakenEl && hideUntakenEl.checked;
+      container.querySelectorAll('[data-gc-mark]').forEach(function(item) {
+        var tick = item.querySelector('.mark-tick');
+        var isTaken = tick && tick.checked;
+        item.style.display = (hideUntaken && !isTaken) ? 'none' : '';
+      });
+    }
+
+    if (hideUntakenEl) {
+      hideUntakenEl.addEventListener('change', updateVisibility);
+    }
+    container.querySelectorAll('.mark-tick').forEach(function(tick) {
+      tick.addEventListener('change', updateVisibility);
+    });
+    updateVisibility();
+  }
+
+  function updateButton(btn, crossed) {
+    btn.classList.toggle('is-crossed', crossed);
+    btn.title = crossed ? 'Un-cross this mark' : 'Cross off this mark';
+  }
+
+  window.CardInitializers = window.CardInitializers || {};
+  window.CardInitializers['ghost-consequences'] = initializeGhostConsequences;
+})();
